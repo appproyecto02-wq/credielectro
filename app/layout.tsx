c7c2188b8aa7cc6592e257752c943b0c3ab5a774
dyn <credielectrodyn@gmail.com>
2026-03-17 15:35:58 -0300

    fix PWA manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <script dangerouslySetInnerHTML={{
+  __html: `
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js')
+      })
+    }
+  `
+}} />
         {children}
       </body>
     </html>
